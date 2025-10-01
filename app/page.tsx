d4012b8f0402ed8ef0325373c870bff95a08bf1f
Fernando Pereira <comprasdfernando@gmail.com>
2025-10-01 00:58:24 -0300

    add banner cadastrar

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,16 @@
 "use client";
 
-import Link from "next/link";
+import Carrossel from "../components/Carrossel";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center justify-center flex-grow text-center p-6">
-      {/* Título */}
-      <h1 className="text-2xl font-bold text-gray-800 mb-6">
+    <div className="flex flex-col items-center justify-center text-center">
+      <h1 className="text-2xl font-bold text-gray-800 mt-6">
         Bem-vindo à IA Drogarias
       </h1>
 
-      {/* Botões principais */}
-      <div className="flex flex-col space-y-4 w-full max-w-xs">
-        <Link
-          href="/servicos"
-          className="bg-blue-600 text-white py-4 rounded-lg shadow-lg hover:bg-blue-700 transition text-lg font-semibold text-center"
-        >
-          Solicitar Serviço Farmacêutico
-        </Link>
-
-        <Link
-          href="/farmacia"
-          className="bg-green-600 text-white py-4 rounded-lg shadow-lg hover:bg-green-700 transition text-lg font-semibold text-center"
-        >
-          Comprar Medicamento
-        </Link>
-      </div>
+      {/* Carrossel */}
+      <Carrossel />
     </div>
   );
 }
