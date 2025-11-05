42bff8324ac707d2ba0757d1e50b3ec0e2f6eeba
Fernando Pereira <comprasdfernando@gmail.com>
2025-11-05 00:53:15 -0300

    correção do caminho para as paginas

--- app/root-client-layout.tsx
+++ app/root-client-layout.tsx
@@ -75,14 +75,14 @@ export default function RootClientLayout({ children }: { children: React.ReactNo
           {/* Menu Desktop */}
           <nav className="hidden sm:flex items-center gap-3">
             <Link
-              href="/drogariaredefabiano"
+              href="/drogaria/drogariaredefabiano"
               className="px-4 py-2 bg-white text-blue-700 rounded-lg shadow hover:bg-gray-100 transition text-sm font-medium"
             >
               D REDE FABIANO
             </Link>
 
             <Link
-              href="/drogariaredefabiano/pdv"
+              href="/drogaria/drogariaredefabiano/pdv"
               className="px-4 py-2 bg-white text-blue-700 rounded-lg shadow hover:bg-gray-100 transition text-sm font-medium"
             >
               PDV
