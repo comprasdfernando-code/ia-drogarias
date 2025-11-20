9fa06728760ed54f7697c0b660624a1f19d3dec4
comprasdfernando-code <comprasdfernando@gmail.com>
2025-11-20 00:17:42 -0300

    fix path globals.css

--- app/turismo/mundoverdetour/layout.tsx
+++ app/turismo/mundoverdetour/layout.tsx
@@ -1,5 +1,5 @@
 import { Poppins } from "next/font/google";
-import "../globals.css"; // garante tailwind
+import "@/app/globals.css"; // garante tailwind
 
 const poppins = Poppins({
   subsets: ["latin"],
