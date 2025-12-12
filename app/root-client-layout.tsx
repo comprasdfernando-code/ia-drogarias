c4f7bb762f5b9ce8c0e97f0bc373ac6ffd9fdc6e
Gisele Pereira <comprasdfernando@gmail.com>
2025-12-12 17:00:11 -0300

    mercado v3

--- app/root-client-layout.tsx
+++ app/root-client-layout.tsx
@@ -14,6 +14,9 @@ export default function RootClientLayout({ children }: { children: React.ReactNo
   if (typeof window !== "undefined" && window.location.pathname.startsWith("/gigante")) {
     return <>{children}</>;
   }
+  if (typeof window !== "undefined" && window.location.pathname.startsWith("/mercados/lojinhadaoportunidades")) {
+    return <>{children}</>;
+  }
   if (typeof window !== "undefined" && window.location.pathname.startsWith("/clinicas/draannedayane")) {
     return <>{children}</>;
   }
