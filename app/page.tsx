7d6d021f35ecdd8b39601857adc673aa07e27db8
Gisele Pereira <comprasdfernando@gmail.com>
2026-01-22 00:09:47 -0300

    home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import PortalStories from "@/components/PortalStories";
-
-export default function HomePage() {
-  return <PortalStories />;
+export default function Home() {
+  redirect("/fv");
 }
