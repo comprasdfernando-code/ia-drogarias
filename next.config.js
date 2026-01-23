af6d2a9d13139448898ea092717d3d14fe1be080
Gisele Pereira <comprasdfernando@gmail.com>
2026-01-23 19:53:36 -0300

    imagens ninho car

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   experimental: {
     esmExternals: true,
   },
+
   images: {
-    domains: ['skzcvpkmcktjryvstcti.supabase.co'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "skzcvpkmcktjryvstctl.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
   },
 };
 
