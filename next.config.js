3b612739211d2f12e511f15cdbd3daa295aa1a4a
Gisele Pereira <comprasdfernando@gmail.com>
2026-01-23 20:54:33 -0300

    imagem

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   experimental: {
     esmExternals: true,
   },
-
   images: {
-    domains: ["skzcvpkmcktjryvstctl.supabase.co"],
+    domains: ["skzcvpkmcktjryvstctl.supabase.co"], // ✅ CORRETO: ctl
   },
 };
 
