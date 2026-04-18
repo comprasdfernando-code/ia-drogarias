a820637efadfaaa240f8041db9882ad2571d588d
Gisele Pereira <comprasdfernando@gmail.com>
2026-04-18 20:48:51 -0300

    iasmin

--- app/root-client-layout.tsx
+++ app/root-client-layout.tsx
@@ -14,6 +14,9 @@ export default function RootClientLayout({ children }: { children: React.ReactNo
   if (typeof window !== "undefined" && window.location.pathname.startsWith("/dayfestas")) {
     return <>{children}</>;
   }
+  if (typeof window !== "undefined" && window.location.pathname.startsWith("/salao/studioiasminsouzanails")) {
+    return <>{children}</>;
+  }
   if (typeof window !== "undefined" && window.location.pathname.startsWith("/formulas")) {
     return <>{children}</>;
   }
